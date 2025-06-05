096c14321fc4523ff435804d5934a175638701be
rhazem13 <rhazem13@yahoo.com>
2025-06-05 09:54:13 +0300

    enhance responisvnes of sidenav

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, MatIconModule],
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
   userRole: string | null = null;
+  isMobileMenuOpen: boolean = false;
+  isMobileView: boolean = window.innerWidth < 768;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.userRole = this.authService.getUserRole();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobileView = window.innerWidth < 768;
+    if (!this.isMobileView) {
+      this.isMobileMenuOpen = false;
+    }
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    if (this.isMobileView) {
+      this.isMobileMenuOpen = false;
+    }
   }
 
   logout(): void {
